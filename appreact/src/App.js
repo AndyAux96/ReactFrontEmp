63988472b27ca3a8cc4b6844602befb0fa12e316
AndyAux96 <andersonaux.96@gmail.com>
2024-02-05 21:28:38 -0500

    Conexión exitosa con back♪

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+
+  const urlApi = "http://localhost:49146/api/employee";
+  const [empleados, setEmpleados] = useState([]);
+
+  useEffect(() => {
+    listEmployee();
+  }, [])
+
+
+
+  //Cargar los empleados con axios
+  const listEmployee = async () => {
+    const datosback = await axios.get(urlApi);
+    console.log(datosback.data);
+    console.log("Empleados cargados");
+    setEmpleados(datosback.data);
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Lista de empleados</h1>
+      <table class="table table-striped table-dark">
+        <thead className="thead-light">
+          <tr>
+            <th>Id </th>
+            <th>Nombre</th>
+            <th>Departamento</th>
+            <th>Dia de ingreso</th>
+            <th>Foto</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            //Iterar el arreglo proveniente del back
+            empleados.map((empleado, indice) => (
+              <tr key={indice} className='table-primary'>
+                <th scope="row">{empleado.EmployeeId}</th>
+                <td>{empleado.EmployeeName}</td>
+                <td>{empleado.Departament}</td>
+                <td>{new Date(empleado.DateOfJoining).toLocaleString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'})}</td>
+                <td>{empleado.PhotoFileName}</td>
+                
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
-
+// <Link to={`/editar/${empleado.idEmpleado}`} n className='btn btn-warning btn-sm me-3'>Editar</Link>
 export default App;
